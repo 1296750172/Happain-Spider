76b9e1044695c0dc6183a10e5fc7bfab317ae922
happain <1296750172@qq.com>
2021-12-06 01:25:41 +0800

    完成zapa爬虫第二版 1600

--- zaba爬取/main.py
+++ zaba爬取/main.py
@@ -4,32 +4,9 @@ import time
 import requests
 import re
 from lxml import etree
-
-import csv
-
-
-class MyCsv():
-    def __init__(self):
-        pass
-
-
-    @staticmethod
-    def read(path,encoding='utf-8'):
-        with open(path,'r',encoding=encoding) as f:
-            data=list(csv.reader(f))
-        return data
-
-    @staticmethod
-    def writerow(file,data):
-        write=csv.writer(file)
-        write.writerow(data)
-        pass
-
-    @staticmethod
-    def writerows(file,data):
-        write=csv.writer(file)
-        write.writerows(data)
-        pass
+import configparser
+import xlrd
+from xlutils.copy import copy
 
 
 
@@ -98,44 +75,70 @@ def get_text(url):
     return name,price,bianhao,mianliao,cicun,imglist
 
 
+# 读取配置
+def readConfig():
+    cf = configparser.ConfigParser()
+    cf.read("config.ini",encoding='utf-8')
+    imgpath = cf.items('config')
+    return imgpath[0][1]
+
+
+
+
+def write_excel_xls_append(path, value):
+    index = len(value)  # 获取需要写入数据的行数
+    workbook = xlrd.open_workbook(path)  # 打开工作簿
+    sheets = workbook.sheet_names()  # 获取工作簿中的所有表格
+    worksheet = workbook.sheet_by_name(sheets[0])  # 获取工作簿中所有表格中的的第一个表格
+    rows_old = worksheet.nrows  # 获取表格中已存在的数据的行数
+    new_workbook = copy(workbook)  # 将xlrd对象拷贝转化为xlwt对象
+    new_worksheet = new_workbook.get_sheet(0)  # 获取转化后工作簿中的第一个表格
+    for i in range(0, index):
+        for j in range(0, len(value[i])):
+            new_worksheet.write(i+rows_old, j, value[i][j])  # 追加写入数据，注意是从i+rows_old行开始写入
+    new_workbook.save(path)  # 保存工作簿
+    print("xls格式表格【追加】写入数据成功！")
+
+
 
 # 输入页面信息
 def headUrl(url):
     temurl ="https://www.zara.cn/cn/zh/{}-p{}.html"
     headers= {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
     res = requests.get(url=url,headers= headers)
-    data = res.json()['productGroups'][0]['elements'][0]['commercialComponents']
+    data = res.json()['productGroups'][0]['elements']
     for i in data:
-        name = i['seo']['keyword']
-        proid = i['seo']['seoProductId']
+        name = i['commercialComponents'][0]['seo']['keyword']
+        proid = i['commercialComponents'][0]['seo']['seoProductId']
         prourl = temurl.format(name,proid)
         if not quchong(proid):
             print(prourl)
             prodata = get_text(prourl)
             if prodata is None:
                 continue
-            print(prodata)
             time.sleep(1)
             # 下载图片
-            if not os.path.exists("./image/{}".format(proid)):
-                os.mkdir("./image/"+proid)
             for j in enumerate(prodata[-1]):
-                downloadImg(proid,j[1],j[0])
-            # MyCsv.writerow(f, ['名称', '编号', '价格', '材质', '尺寸', '图片id'])
-            # name price, bianhao, mianliao, cicun, imglist
-            with open('result.csv', 'a',encoding='utf-8',newline='') as f:
-                MyCsv.writerow(f, [prodata[0], prodata[2], prodata[1], prodata[3], prodata[4], proid])
+                downloadImg(proid,prodata,j[1],j[0])
+
+            write_excel_xls_append(path="result.xls", value=[[prodata[0], prodata[2], prodata[1], prodata[3], prodata[4], proid]])
 
 
 
 
 # 下载图片
-def downloadImg(sku,url,num):
-    headers= {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
-    with open('./image/{}/{}.png'.format(sku,num),'wb') as f:
+def downloadImg(proid,obj,url,num):
+    tem = '{}_{}_{}_{}_{}'.format(proid,obj[1].replace("¥",'').strip(),obj[3].replace("\n",''),obj[4],num)
+    headers= {
+        'cookie':'optimizelyEndUserId=oeu1637356690393r0.7057703069582593; '
+                 '_abck=893741F99DB3854303ED009168895D2F~0~YAAQRQ1x3wJTFhd9AQAAawy8cAY789FhSRXX4vK5ic720CJdS7vy32Ld6TzjZ8wmMT5NCj/t7dMA17wbuWX4ak8h9oIArQ9LJNpEhoep76QKUxtf13eVoCn8vRK6D4wiCh4iTlj17mszH8O0YmA/ygOORPTTXJbz0DARnDrQn3ADzYFSK0HMa6dEkRQ2Kv0z8WfHLvyNz3dD4At+cDOKrAyiyeozPnBl5cNQCtNPKew1AuVfNHnNZT1f0aefrKwDRmJgYu2LSqNAcbJpn5Dz3sJWjjE4g375ewDcM0Rw39UvB4cY78Yv7KuepGMMZ6GSkE4gYJBGkE8kpopcHgt5N9PcdftGb85ZmttCxOxYX3VOUQdUthJvptt46cab0hEqksNKtfdn27eMALrFZruHNZbbMVrT~-1~-1~-1; _ga=GA1.2.2046539325.1637356694; RT="z=1&dm=zara.cn&si=27af5e6b-adba-44f8-908b-2d366b4077f9&ss=kwma8ihf&sl=7&tt=36d&bcn=%2F%2F684d0d4c.akstat.io%2F&ul=fkwo&hd=fkwy"; _ga_HCEXQGE0MW=GS1.1.1638287766.12.1.1638288492.0',
+        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
+    if not os.path.exists('./image/'+imgpath):
+        os.mkdir('./image/'+imgpath)
+    with open('./image/{}/{}.jpg'.format(imgpath,tem),'wb') as f:
         res = requests.get(url=url,headers= headers)
         f.write(res.content)
-        print('{}-{}图片下载完成'.format(sku,num))
+        print(tem+'图片下载完成')
         time.sleep(0.1)
 
 # 去重函数
@@ -161,13 +164,18 @@ def getlink():
         data = [i.replace("\n",'') for i in f.readlines()]
     return data
 
+def test():
+    data = downloadImg("08614577","https://static.zara.cn/photos///2021/I/0/3/p/8614/577/506/446/w/563/8614577506_1_1_1.jpg?ts=1638442880235","1")
+    print()
+
+    pass
+imgpath = readConfig()
 if __name__ == '__main__':
-    if  int(time.time())>1638454981:
+    if  int(time.time())>1639070569:
         exit()
+
     reslink = getlink()
-    if not os.path.exists("./result.csv"):
-        with open('result.csv', 'a', encoding='utf-8', newline='') as f:
-            MyCsv.writerow(f, ['名称', '编号', '价格', '材质', '尺寸', '图片id'])
+
     for i in reslink:
         try:
             num =re.compile('v1=(.*)').findall(i)[0]
