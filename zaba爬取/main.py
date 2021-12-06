4ed9ef3724dc3629b50b02aadf4feff7ad236a50
happain <1296750172@qq.com>
2021-12-07 00:11:16 +0800

    完成zapa爬虫第三版 1600

--- zaba爬取/main.py
+++ zaba爬取/main.py
@@ -5,9 +5,58 @@ import requests
 import re
 from lxml import etree
 import configparser
-import xlrd
 from xlutils.copy import copy
+import xlwt
+import xlrd
+import datetime
 
+class Excel_write():
+    def __init__(self,path):
+        self.path=path
+        self.ex=xlwt.Workbook()
+
+        pass
+
+
+    def add_sheet(self,name,data_list,col=-1):
+
+        sheet_list=[]
+        if type(name)==list:
+            for i in name:
+                sh=self.ex.add_sheet(i, cell_overwrite_ok=True)
+                if col!=-1:
+                    self.set_width(sh,len(data_list),col)
+                self.write(sh,0,data_list)
+
+                sheet_list.append(sh)
+
+            return sheet_list
+        else:
+            sh = self.ex.add_sheet(name, cell_overwrite_ok=True)
+            if col != -1:
+                self.set_width(sh, len(data_list), col)
+            self.writes(sh, 0, data_list)
+            return  sh
+
+
+
+    def write(self, sheet, hang, data, index):
+        for i in zip(index, data):
+            sheet.write(hang, i[0], i[1])
+
+
+    def writes(self,sheet,hang,data):
+        for i in range(0,len(data)):
+            sheet.write(hang,i,data[i])
+
+
+    def set_width(self,sh,long_num,width):
+        for i in range(0,long_num):
+            sh.col(i).width = 256 * width
+
+
+    def save(self):
+        self.ex.save(self.path)
 
 
 
@@ -80,7 +129,10 @@ def readConfig():
     cf = configparser.ConfigParser()
     cf.read("config.ini",encoding='utf-8')
     imgpath = cf.items('config')
-    return imgpath[0][1]
+    data_dict = {}
+    for i in imgpath:
+        data_dict[i[0]] = i[1]
+    return data_dict
 
 
 
@@ -103,10 +155,16 @@ def write_excel_xls_append(path, value):
 
 # 输入页面信息
 def headUrl(url):
+    excel = Excel_write('result.xls')
+    sheet = excel.add_sheet("demo",['名称','编号','价格','材质','尺寸','图片id','链接'],15)
+    hang = 1
+
+
     temurl ="https://www.zara.cn/cn/zh/{}-p{}.html"
     headers= {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
     res = requests.get(url=url,headers= headers)
     data = res.json()['productGroups'][0]['elements']
+
     for i in data:
         name = i['commercialComponents'][0]['seo']['keyword']
         proid = i['commercialComponents'][0]['seo']['seoProductId']
@@ -120,22 +178,23 @@ def headUrl(url):
             # 下载图片
             for j in enumerate(prodata[-1]):
                 downloadImg(proid,prodata,j[1],j[0])
-
-            write_excel_xls_append(path="result.xls", value=[[prodata[0], prodata[2], prodata[1], prodata[3], prodata[4], proid]])
+            excel.writes(sheet,hang,[prodata[0], prodata[2], prodata[1], prodata[3], prodata[4], proid,prourl])
+            # write_excel_xls_append(path="result.xls", value=[[prodata[0], prodata[2], prodata[1], prodata[3], prodata[4], proid]])
+    excel.save()
 
 
 
 
 # 下载图片
 def downloadImg(proid,obj,url,num):
-    tem = '{}_{}_{}_{}_{}'.format(proid,obj[1].replace("¥",'').strip(),obj[3].replace("\n",''),obj[4],num)
+    tem = '{}_{}_{}_{}_{}'.format(proid,obj[1].replace("¥",'').strip(),obj[3].replace("\n",''),obj[4].replace("/",'').replace("\\",'').replace('"','').replace("'",''),num)
     headers= {
         'cookie':'optimizelyEndUserId=oeu1637356690393r0.7057703069582593; '
                  '_abck=893741F99DB3854303ED009168895D2F~0~YAAQRQ1x3wJTFhd9AQAAawy8cAY789FhSRXX4vK5ic720CJdS7vy32Ld6TzjZ8wmMT5NCj/t7dMA17wbuWX4ak8h9oIArQ9LJNpEhoep76QKUxtf13eVoCn8vRK6D4wiCh4iTlj17mszH8O0YmA/ygOORPTTXJbz0DARnDrQn3ADzYFSK0HMa6dEkRQ2Kv0z8WfHLvyNz3dD4At+cDOKrAyiyeozPnBl5cNQCtNPKew1AuVfNHnNZT1f0aefrKwDRmJgYu2LSqNAcbJpn5Dz3sJWjjE4g375ewDcM0Rw39UvB4cY78Yv7KuepGMMZ6GSkE4gYJBGkE8kpopcHgt5N9PcdftGb85ZmttCxOxYX3VOUQdUthJvptt46cab0hEqksNKtfdn27eMALrFZruHNZbbMVrT~-1~-1~-1; _ga=GA1.2.2046539325.1637356694; RT="z=1&dm=zara.cn&si=27af5e6b-adba-44f8-908b-2d366b4077f9&ss=kwma8ihf&sl=7&tt=36d&bcn=%2F%2F684d0d4c.akstat.io%2F&ul=fkwo&hd=fkwy"; _ga_HCEXQGE0MW=GS1.1.1638287766.12.1.1638288492.0',
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
-    if not os.path.exists('./image/'+imgpath):
-        os.mkdir('./image/'+imgpath)
-    with open('./image/{}/{}.jpg'.format(imgpath,tem),'wb') as f:
+    if not os.path.exists('./image/'+data_dict['imgpath']):
+        os.mkdir('./image/'+data_dict['imgpath'])
+    with open('./image/{}/{}.jpg'.format(data_dict['imgpath'],tem),'wb') as f:
         res = requests.get(url=url,headers= headers)
         f.write(res.content)
         print(tem+'图片下载完成')
@@ -159,27 +218,9 @@ def quchong(data):
 
 
 
-def getlink():
-    with open('link.txt','r') as f:
-        data = [i.replace("\n",'') for i in f.readlines()]
-    return data
 
-def test():
-    data = downloadImg("08614577","https://static.zara.cn/photos///2021/I/0/3/p/8614/577/506/446/w/563/8614577506_1_1_1.jpg?ts=1638442880235","1")
-    print()
-
-    pass
-imgpath = readConfig()
+data_dict = readConfig()
 if __name__ == '__main__':
-    if  int(time.time())>1639070569:
-        exit()
-
-    reslink = getlink()
-
-    for i in reslink:
-        try:
-            num =re.compile('v1=(.*)').findall(i)[0]
-        except Exception as e:
-            continue
-        urllist = "https://www.zara.cn/cn/zh/category/{}/products?ajax=true".format(num)
-        headUrl(url=urllist)
+    num =re.compile('v1=(.*)').findall(data_dict['link'])[0]
+    urllist = "https://www.zara.cn/cn/zh/category/{}/products?ajax=true".format(num)
+    headUrl(url=urllist)
